836f9b21e236b9b4d59a98c86884d70519a56c34
Kevin Amado <kamadorueda@gmail.com>
2019-11-07 17:45:11 -0500

    feat(deploy): change the versioning system

--- setup.py
+++ setup.py
@@ -3,14 +3,27 @@
 import distutils
 from datetime import datetime
 
+
+def get_minor_version() -> int:
+    """Number of seconds since the beginning of the month."""
+    utc_now = \
+        datetime.utcnow()
+    utc_beginning_of_month = \
+        datetime.utcnow().replace(day=1, minute=0, second=0)
+    return int((utc_now - utc_beginning_of_month).total_seconds())
+
+
+def get_version():
+    return datetime.utcnow().strftime(f'%Y.%m.{get_minor_version()}')
+
+
 distutils.core.setup(
     name='oblivion',
-    version=datetime.utcnow().strftime('%Y.%m.%d.%H.%M.%S'),
+    version=get_version(),
     packages=[
         'oblivion',
         'oblivion.cli'
     ],
-
     install_requires=[
     ],
     entry_points={
